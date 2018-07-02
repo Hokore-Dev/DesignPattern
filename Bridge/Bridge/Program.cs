f42b5a3eb790b2cca878a5c037cc071ccc11ad50
haminjun0 <haminjun0@gmail.com>
2018-07-02 18:45:28 +0900

    Bridge 패턴 공부 추가

---
+++ Bridge/Bridge/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Bridge
+{
+    interface Weapon
+    {
+        void attack();
+        void repair();
+    }
+
+    class Bow : Weapon
+    {
+        public void attack()
+        {
+        }
+        public void repair()
+        {
+        }
+    }
+
+    class Sword : Weapon
+    {
+        public void attack()
+        {
+        }
+        public void repair()
+        {
+        }
+    }
+
+    interface WeaponHandler
+    {
+        void handle();
+    }
+
+    class Warrior : WeaponHandler
+    {
+        public void handle()
+        {
+
+        }
+    }
+
+    class Smith : WeaponHandler
+    {
+        public void handle()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

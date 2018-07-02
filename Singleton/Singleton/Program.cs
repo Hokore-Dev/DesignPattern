b92200647df91f9c314cc86ea0fb753be14de6e7
haminjun0 <haminjun0@gmail.com>
2018-07-02 18:10:35 +0900

    단일체 패턴 공부 추가

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+        public int A = 0;
+
+        private Singleton() { }
+
+        public static Singleton getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton.getInstance().A = 4;
+        }
+    }
+}

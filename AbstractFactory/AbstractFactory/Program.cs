3e0299c442cb4f8aeb32f0038304abf363d2ae91
minjun.ha <minjun.ha@sundaytoz.com>
2018-07-02 15:56:34 +0900

    추상 팩토리 패턴 공부 추가

---
+++ AbstractFactory/AbstractFactory/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace AbstractFactory
+{
+    interface IGUIFactory
+    {
+        IButton CreateButton();
+        IToggle CreateToggle();
+    }
+
+    class WinFactory : IGUIFactory
+    {
+        public IButton CreateButton()
+        {
+            return new WinButton();
+        }
+
+        public IToggle CreateToggle()
+        {
+            return new WinToggle();
+        }
+    }
+
+    class OSXFactory : IGUIFactory
+    {
+        public IButton CreateButton()
+        {
+            return new OSXButton();
+        }
+
+        public IToggle CreateToggle()
+        {
+            return new OSXToggle();
+        }
+    }
+
+    interface IButton
+    {
+        void Paint();
+    }
+
+    class WinButton : IButton
+    {
+        public void Paint()
+        {
+        }
+    }
+
+    class OSXButton : IButton
+    {
+        public void Paint()
+        {
+
+        }
+    }
+
+    interface IToggle
+    {
+        void On();
+    }
+
+    class WinToggle : IToggle
+    {
+        public void On()
+        {
+
+        }
+    }
+
+    class OSXToggle : IToggle
+    {
+        public void On()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IGUIFactory factory = new WinFactory();
+            var button = factory.CreateButton();
+            button.Paint();
+        }
+    }
+}

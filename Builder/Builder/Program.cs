fe53659f6ff9a5f75a98c38772691a990038b3eb
haminjun0 <haminjun0@gmail.com>
2018-07-02 17:09:17 +0900

    빌더 패턴 공부 추가

---
+++ Builder/Builder/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Pizza
+    {
+        string dough;
+        string sauce;
+        string topping;
+        public Pizza() { }
+        public void SetDough(string inDough) { dough = inDough; }
+        public void SetSauce(string inSauce) { sauce = inSauce; }
+        public void SetTopping(string inTopping) { topping = inTopping; }
+    }
+
+    abstract class PizzaBuilder
+    {
+        protected Pizza pizza;
+        public PizzaBuilder() { }
+        public Pizza GetPizza() { return pizza; }
+        public void CreateNewPizza() { pizza = new Pizza(); }
+
+        public abstract void BuildDough();
+        public abstract void BuildSauce();
+        public abstract void BuildTopping();
+    }
+
+    class HawaiianPizzaBuilder : PizzaBuilder
+    {
+        public override void BuildDough() { pizza.SetDough("cross"); }
+        public override void BuildSauce() { pizza.SetSauce("mild"); }
+        public override void BuildTopping() { pizza.SetTopping("ham+pineapple"); }
+    }
+
+    class SpicyPizzaBuilder : PizzaBuilder
+    {
+        public override void BuildDough() { pizza.SetDough("pan baked"); }
+        public override void BuildSauce() { pizza.SetSauce("hot"); }
+        public override void BuildTopping() { pizza.SetTopping("pepparoni+salami"); }
+    }
+
+    class Waiter
+    {
+        private PizzaBuilder pizzaBuilder;
+
+        public void SetPizzaBuilder(PizzaBuilder inPizzaBuilder) { pizzaBuilder = inPizzaBuilder; }
+        public Pizza GetPizza() { return pizzaBuilder.GetPizza(); }
+
+        public void ConstructPizza()
+        {
+            pizzaBuilder.CreateNewPizza();
+            pizzaBuilder.BuildDough();
+            pizzaBuilder.BuildSauce();
+            pizzaBuilder.BuildTopping();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Waiter waiter = new Waiter();
+            PizzaBuilder hawaiianPizzaBuilder = new HawaiianPizzaBuilder();
+            PizzaBuilder spicyPizzaBuilder = new SpicyPizzaBuilder();
+
+            waiter.SetPizzaBuilder(hawaiianPizzaBuilder);
+            waiter.ConstructPizza();
+
+            Pizza pizza = waiter.GetPizza();
+        }
+    }
+}

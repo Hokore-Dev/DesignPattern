5be62648c26f750d0833ef920b33f7b40c92c019
minjun.ha <haminjun0@gmail.com>
2018-07-03 22:15:37 +0900

    프록시 패턴 공부 추가

---
+++ Proxy/Proxy/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Proxy
+{
+    class MainClass
+    {
+        interface IImage
+        {
+            void Display();
+        }
+
+        class RealImage : IImage
+        {
+            public string FileName { get; private set; }
+
+            public RealImage(string fileName)
+            {
+                FileName = fileName;
+                LoadFromFile();
+            }
+
+            private void LoadFromFile()
+            {
+                Console.WriteLine("Loading " + FileName);
+            }
+
+            public void Display()
+            {
+                Console.WriteLine("Displaying " + FileName);
+            }
+        }
+
+        class ProxyImage : IImage
+        {
+            public string FileName { get; private set; }
+            private IImage image;
+
+            public ProxyImage(string fileName)
+            {
+                FileName = fileName;
+            }
+
+            public void Display()
+            {
+                if (image == null)
+                    image = new RealImage(FileName);
+                image.Display();
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            IImage image = new ProxyImage("HiRes_Image");
+            for (int i = 0; i < 10; i++)
+                image.Display();
+        }
+    }
+}

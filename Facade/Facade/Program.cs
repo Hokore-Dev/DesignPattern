1915d5881015a89a829e8a03faadf02495add165
minjun.ha <haminjun0@gmail.com>
2018-07-03 21:18:54 +0900

    퍼사드 패턴 공부 추가

---
+++ Facade/Facade/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Facade
+{
+    class SubSystemOne
+    {
+        public void MethodOne()
+        {
+            Console.WriteLine("SubSystemOne Method");
+        }
+    }
+
+    class SubSystemTwo
+    {
+        public void MethodTwo()
+        {
+            Console.WriteLine("SubSystemTwo Method");
+        }
+    }
+
+    class SubSystemThree
+    {
+        public void MethodThree()
+        {
+            Console.WriteLine("SubSystemThree Method");
+        }
+    }
+
+    class SubSystemFour
+    {
+        public void MethodFour()
+        {
+            Console.WriteLine("SubSystemFour Method");
+        }
+    }
+
+    class Facade
+    {
+        SubSystemOne one;
+        SubSystemTwo two;
+        SubSystemThree three;
+        SubSystemFour four;
+
+        public Facade()
+        {
+            one = new SubSystemOne();
+            two = new SubSystemTwo();
+            three = new SubSystemThree();
+            four = new SubSystemFour();
+        }
+
+        public void MethodA()
+        {
+            Console.WriteLine("\nMethodA() ---- ");
+            one.MethodOne();
+            two.MethodTwo();
+            four.MethodFour();
+        }
+
+        public void MethodB()
+        {
+            Console.WriteLine("\nMethodB() ---- ");
+            two.MethodTwo();
+            three.MethodThree();
+        }
+    }
+
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

77dcec6b33d2656e3bd9dd2994e9b982d15391c3
haminjun0 <haminjun0@gmail.com>
2018-07-04 14:17:13 +0900

    해석자 패턴 공부 추가

---
+++ Interpreter/Interpreter/Expression.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter
+{
+    abstract class Expression
+    {
+        public Expression Next
+        {
+            get;
+            set;
+        }
+        public Expression()
+        {
+            Next = null;
+        }
+        public abstract string Interpret(string context);
+        public virtual void DoItWithPicture(Picture picture)
+        {
+            if (Next != null)
+            {
+                Next.DoItWithPicture(picture);
+            }
+        }
+        protected string NextInterpret(string context)
+        {
+            if (Next != null)
+            {
+                return Next.Interpret(context);
+            }
+            return context;
+        }
+        protected int GetNumber(string context, int index)
+        {
+            int index2 = -1;
+            index2 = context.IndexOf(";", index);
+            string re = context.Substring(index + 1, index2 - index - 1);
+            try
+            {
+                return int.Parse(re);
+            }
+            catch { }
+            return 0;
+        }
+    }
+
+    class ToneExpression : Expression
+    {
+        int value;
+        public override string Interpret(string context)
+        {
+            value = 0;
+            int index = -1;
+            string be = "";
+            string af = "";
+            while ((index = context.IndexOf("T")) != -1)
+            {
+                value += GetNumber(context, index);
+                if (index > 0)
+                {
+                    be = context.Substring(0, index);
+                }
+                else
+                {
+                    be = "";
+                }
+                index = context.IndexOf(";", index);
+                af = context.Substring(index + 1);
+                context = be + af;
+            }
+            return NextInterpret(context);
+        }
+        public override void DoItWithPicture(Picture picture)
+        {
+            picture.ChangeTone(value);
+            base.DoItWithPicture(picture);
+        }
+    }
+
+    class BrighExpression : Expression
+    {
+        int value;
+        public override string Interpret(string context)
+        {
+            value = 0;
+            int index = -1;
+            string be = "";
+            string af = "";
+            while ((index = context.IndexOf("B")) != -1)
+            {
+                value += GetNumber(context, index);
+                if (index > 0)
+                {
+                    be = context.Substring(0, index);
+                }
+                else
+                {
+                    be = "";
+                }
+                index = context.IndexOf(";", index);
+                af = context.Substring(index + 1);
+                context = be + af;
+            }
+            return NextInterpret(context);
+        }
+        public override void DoItWithPicture(Picture picture)
+        {
+            picture.ChangeBrightness(value);
+            base.DoItWithPicture(picture);
+        }
+    }
+
+    class SatuExpression : Expression
+    {
+        int value;
+        public override string Interpret(string context)
+        {
+            value = 0;
+            int index = -1;
+            string be = "";
+            string af = "";
+            while ((index = context.IndexOf("S")) != -1)
+            {
+                value += GetNumber(context, index);
+                if (index > 0)
+                {
+                    be = context.Substring(0, index);
+                }
+                else
+                {
+                    be = "";
+                }
+                index = context.IndexOf(";", index);
+                af = context.Substring(index + 1);
+                context = be + af;
+            }
+            return NextInterpret(context);
+        }
+        public override void DoItWithPicture(Picture picture)
+        {
+            picture.ChangeSaturation(value);
+            base.DoItWithPicture(picture);
+        }
+    }
+}
---
+++ Interpreter/Interpreter/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter
+{
+    class Picture
+    {
+        string name;
+        int tone;
+        int brightness;
+        int saturation;
+        public Picture(string name, int tone, int brightness, int saturation)
+        {
+            this.name = name;
+            this.tone = tone;
+            this.brightness = brightness;
+            this.saturation = saturation;
+        }
+        public void ChangeTone(int tone)
+        {
+            this.tone += tone;
+        }
+        public void ChangeBrightness(int brightness)
+        {
+            this.brightness += brightness;
+        }
+        public void ChangeSaturation(int saturation)
+        {
+            this.saturation += saturation;
+        }
+        public void View()
+        {
+            Console.WriteLine("사진 파일명:{0}", name);
+            Console.WriteLine("    색조:{0} 명도:{1} 채도:{2}",
+                tone, brightness, saturation);
+        }
+    }
+
+    class Macro
+    {
+        Expression head = null;
+        Expression tail = null;
+        public void AddExpression(Expression expression)
+        {
+            if (head != null)
+            {
+                tail.Next = expression;
+                tail = expression;
+            }
+            else
+            {
+                head = tail = expression;
+            }
+        }
+        public void ChangePicture(Picture picture)
+        {
+            head.DoItWithPicture(picture);
+        }
+        public void AddContext(string context)
+        {
+            head.Interpret(context);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression ex1 = new ToneExpression();
+            Expression ex2 = new BrighExpression();
+            Expression ex3 = new SatuExpression();
+            Macro macro = new Macro();
+            macro.AddExpression(ex1);
+            macro.AddExpression(ex2);
+            macro.AddExpression(ex3);
+            macro.AddContext("B 20 ;");
+            macro.AddContext("B 20 ;T-12 ; S 10 ; B10 ;");
+            Picture picture = new Picture("현충사의 봄", 100, 100, 100);
+            macro.ChangePicture(picture);
+            picture.View();
+        }
+    }
+}

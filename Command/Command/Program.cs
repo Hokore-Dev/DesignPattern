624add456c2f29162951c860b59cb82dac2a5b08
haminjun0 <haminjun0@gmail.com>
2018-07-04 12:36:13 +0900

    커맨드 패턴 공부 추가

---
+++ Command/Command/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    public interface Command
+    {
+        void Execute();
+    }
+
+    public class Light
+    {
+        public void On()
+        {
+
+        }
+    }
+
+    public class LightOnCommand : Command
+    {
+        Light light;
+
+        public LightOnCommand(Light inLight)
+        {
+            this.light = inLight;
+        }
+
+        public void Execute()
+        {
+            light.On();
+        }
+    }
+
+    public class SimpleRemoteControl
+    {
+        Command slot;
+
+        public void SetCommand(Command inSlot)
+        {
+            slot = inSlot;
+        }
+
+        public void ButtonWasPressd()
+        {
+            slot.Execute();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleRemoteControl remote = new SimpleRemoteControl();
+            Light light = new Light();
+            LightOnCommand lightOn = new LightOnCommand(light);
+
+            remote.SetCommand(lightOn);
+            remote.ButtonWasPressd();
+        }
+    }
+}

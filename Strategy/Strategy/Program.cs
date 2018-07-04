ea17678e136342d7bc581ca0c326a30695383327
haminjun0 <haminjun0@gmail.com>
2018-07-04 18:03:49 +0900

    전략 패턴 공부 추가

---
+++ Strategy/Strategy/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    abstract class Strategy
+    {
+        public abstract void AlgorithmInterface();
+    }
+
+    class ConcreteStrategyA : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine(
+              "Called ConcreteStrategyA.AlgorithmInterface()");
+        }
+    }
+
+    class ConcreteStrategyB : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine(
+              "Called ConcreteStrategyB.AlgorithmInterface()");
+        }
+    }
+
+    class ConcreteStrategyC : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine(
+              "Called ConcreteStrategyC.AlgorithmInterface()");
+        }
+    }
+
+    class Context
+    {
+        private Strategy _strategy;
+
+        public Context(Strategy strategy)
+        {
+            this._strategy = strategy;
+        }
+
+        public void ContextInterface()
+        {
+            _strategy.AlgorithmInterface();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context;
+
+            context = new Context(new ConcreteStrategyA());
+            context.ContextInterface();
+
+            context = new Context(new ConcreteStrategyB());
+            context.ContextInterface();
+
+            context = new Context(new ConcreteStrategyC());
+            context.ContextInterface();
+        }
+    }
+}

1d58c2361a3a7d72ad85c2bc5b9baa18abc65ff4
haminjun0 <haminjun0@gmail.com>
2018-07-04 17:51:14 +0900

    상태 패턴 공부 추가

---
+++ State/State/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace State
+{
+    public interface IState
+    {
+        void Attack();
+        void Jump();
+    }
+
+    public class Idle : IState
+    {
+        public void Attack()
+        {
+            Console.WriteLine("idle attack");
+        }
+
+        public void Jump()
+        {
+            Console.WriteLine("idle jump");
+        }
+    }
+
+    public class Run : IState
+    {
+        public void Attack()
+        {
+            Console.WriteLine("run attack");
+        }
+
+        public void Jump()
+        {
+            Console.WriteLine("run jump");
+        }
+    }
+
+    public class Character
+    {
+        IState state;
+
+        public void SetState(IState inState)
+        {
+            state = inState;
+        }
+
+        public void Action()
+        {
+            state.Attack();
+            state.Jump();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Character character = new Character();
+            character.SetState(new Run());
+
+            character.Action();
+
+            character.SetState(new Idle());
+
+            character.Action();
+        }
+    }
+}
